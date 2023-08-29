d63f1af971d93ae0645d269dccc8bebb5cd783bb
Yaroslav Vorobyov <yaroslav-vorobyov@yandex.ru>
2023-08-29 23:21:04 +0300

    update style .\app\server.py

--- PROJECT_final/microservice/app/server.py
+++ PROJECT_final/microservice/app/server.py
@@ -28,15 +28,19 @@ with open('./model/arima.pkl', 'rb') as pkl_file:
 # обработчик запросов на endpoint '/predict'
 @application.route('/predict')
 def predict():
+    # организуем бесконечный цикл
     while True:
+        # выполняем выдачу предсказания
         try:
-            # для скрипта
+            # формируем дату
             features = app.request.args.get('date', default=(datetime.datetime.today() + datetime.timedelta(days=+1)).strftime("%Y-%m-%d"))
             
             # забираем единственное предсказание
             prediction_model_from_file = model_from_file.forecast(features)[-1]
 
+            # выводим результат на странице
             return f'Предсказание на {features}: {prediction_model_from_file:.5f}'
+        # если дата некорректна - сообщение в консоль браузера
         except:
             print('\033[33mЕсли требуется предсказание на конкретную дату, то в адресной строке "http://localhost:5000/predict?date=YYYY-MM-DD", '\
                 f'где date в формате год-месяц-день\033[0m')
